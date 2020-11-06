6876a0cecd9f5a366c66105995b34f12a6908141
Tycho Bokdam <tycho@palmtreecoding.com>
2020-11-06 14:52:54 +0100

    feat: Export all interfaces

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { ThemeProvider, withTheme, useTheme } from 'theming'
 export * from './create-use-styles'
 export * from './with-styles'
+export * from './utils/interfaces'
 
 export {
   ThemeProvider,
